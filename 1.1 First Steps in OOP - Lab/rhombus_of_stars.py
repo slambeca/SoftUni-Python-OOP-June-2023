e44a47831b9b3cd440e61c996142f4124c7d62b1
Borislav Ivanov <slambeca@abv.bg>
2023-06-08 16:15:42 +0300

    Fourth solution added

--- 1.1 First Steps in OOP - Lab/rhombus_of_stars.py
+++ 1.1 First Steps in OOP - Lab/rhombus_of_stars.py
@@ -47,3 +47,25 @@ draw_rhombus(int(input()))
 #
 # number = int(input())
 # print_rhombus(number)
+
+
+# Variant 4
+
+
+# def get_line(i, n):
+#     spaces_count = n - 1 - i
+#     stars_count = i + 1
+#     return " " * spaces_count + ("* " * stars_count).strip()
+#
+#
+# def print_line(n):
+#     print(get_line(n - 1, n - 1))
+#
+#
+# def print_rhombus(n):
+#     [print(get_line(i, n)) for i in range(n)]
+#     [print(get_line(i, n)) for i in range(n - 2, -1, -1)]
+#
+#
+# number = int(input())
+# print_rhombus(number)
