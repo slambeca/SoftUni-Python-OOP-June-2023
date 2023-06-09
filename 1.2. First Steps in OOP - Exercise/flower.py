d30192bf16f1982dbda230b991adb0b294aa062c
Borislav Ivanov <slambeca@abv.bg>
2023-06-09 17:03:27 +0300

    Update flower.py

--- 1.2. First Steps in OOP - Exercise/flower.py
+++ 1.2. First Steps in OOP - Exercise/flower.py
@@ -1,17 +1,15 @@
 class Flower:
     def __init__(self, name, water_requirements):
-        self.name: str = name
-        self.water_requirements: int = water_requirements
+        self.name = name
+        self.water_requirements = water_requirements
         self.is_happy = False
 
-    def water(self, quantity: int):
-        if quantity >= self.water_requirements:
-            self.is_happy = True
+    def water(self, quantity):
+        self.is_happy = quantity >= self.water_requirements
 
     def status(self):
         if self.is_happy:
             return f"{self.name} is happy"
-
         return f"{self.name} is not happy"
 
 
