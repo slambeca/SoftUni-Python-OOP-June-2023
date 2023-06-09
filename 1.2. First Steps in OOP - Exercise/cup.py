a4c2f8a349c8a010a8cec4523f73dc49d379ad03
Borislav Ivanov <slambeca@abv.bg>
2023-06-09 16:41:22 +0300

    Second solution added

--- 1.2. First Steps in OOP - Exercise/cup.py
+++ 1.2. First Steps in OOP - Exercise/cup.py
@@ -16,3 +16,16 @@ print(cup.status())
 cup.fill(40)
 cup.fill(20)
 print(cup.status())
+
+# Variant 2
+# class Cup:
+#     def __init__(self, size, quantity):
+#         self.size = size
+#         self.quantity = quantity
+
+#     def status(self):
+#         return self.size - self.quantity
+
+#     def fill(self, milliliters):
+#         if self.status() >= milliliters:
+#             self.quantity += milliliters
