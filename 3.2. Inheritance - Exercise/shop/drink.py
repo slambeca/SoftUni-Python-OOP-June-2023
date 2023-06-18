6c50e1c57823d21fc1cb0c18aa7665c40ac00286
Borislav Ivanov <slambeca@abv.bg>
2023-06-18 14:47:32 +0300

    Add files via upload

---
+++ 3.2. Inheritance - Exercise/shop/drink.py
@@ -0,0 +1,9 @@
+from project.product import Product
+
+
+class Drink(Product):
+    DEFAULT_QUANTITY = 10
+
+    def __init__(self, name):
+        super().__init__(name, self.DEFAULT_QUANTITY)
+        # We are passing the constant 10 to the __init__
