bcfd5e752823cb2627656104762566937be64f36
Borislav Ivanov <slambeca@abv.bg>
2023-06-20 11:55:02 +0300

    Update shop.py

--- 5.1. Class and Static Methods - Lab/shop.py
+++ 5.1. Class and Static Methods - Lab/shop.py
@@ -6,13 +6,13 @@ class Shop:
         self.items = {}
 
     @classmethod
-    def small_shop(cls, name, type, capacity=10):
-        return cls(name, type, capacity)
+    def small_shop(cls, name, type):
+        return cls(name, type, 10)
 
     def add_item(self, item_name):
         if item_name not in self.items.keys():
             self.items[item_name] = 0
-        if self.items[item_name] == self.capacity:
+        if sum(self.items.values()) == self.capacity:
             return "Not enough capacity in the shop"
         self.items[item_name] += 1
         return f"{item_name} added to the shop"
