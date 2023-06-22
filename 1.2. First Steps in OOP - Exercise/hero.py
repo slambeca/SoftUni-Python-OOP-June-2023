35034c873e0694e2ca53062503875e9f4c52adf4
Borislav Ivanov <slambeca@abv.bg>
2023-06-22 13:21:59 +0300

    Second solution added

--- 1.2. First Steps in OOP - Exercise/hero.py
+++ 1.2. First Steps in OOP - Exercise/hero.py
@@ -19,3 +19,17 @@ print(hero.defend(50))
 hero.heal(50)
 print(hero.defend(99))
 print(hero.defend(1))
+
+# Variant 2
+# class Hero:
+#     def __init__(self, name, health):
+#         self.name = name
+#         self.health = health
+# 
+#     def defend(self, damage):
+#         self.health = max(self.health - damage, 0)
+#         if self.health == 0:
+#             return f"{self.name} was defeated"
+# 
+#     def heal(self, amount):
+#         self.health += amount
