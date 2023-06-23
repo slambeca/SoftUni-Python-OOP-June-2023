3ed334511e9c68215782fd446774ced0c1f8bb1f
Borislav Ivanov <slambeca@abv.bg>
2023-06-23 18:12:33 +0300

    Update dog.py

--- 3.1. Inheritance - Lab/single_inheritance/dog.py
+++ 3.1. Inheritance - Lab/single_inheritance/dog.py
@@ -4,3 +4,12 @@ from project.animal import Animal
 class Dog(Animal):
     def bark(self):
         return "barking..."
+
+# Variant 2
+# from project.animal import Animal
+
+
+# class Dog(Animal):
+#     @staticmethod
+#     def bark():
+#         return "barking..."
