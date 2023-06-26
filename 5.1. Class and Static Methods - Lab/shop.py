f32bae6278063d073ae416be6d46604bf7a28c15
Borislav Ivanov <slambeca@abv.bg>
2023-06-26 18:23:10 +0300

    Second solution added

--- 5.1. Class and Static Methods - Lab/shop.py
+++ 5.1. Class and Static Methods - Lab/shop.py
@@ -42,3 +42,38 @@ print(small_shop.add_item("Jeans"))
 
 print(small_shop.remove_item("Jeans", 2))
 print(small_shop.items)
+
+# Variant 2
+class Shop:
+    def __init__(self, name, type, capacity):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name, type):
+        return cls(name, type, capacity=10)
+
+    def add_item(self, item_name):
+        if not self.capacity:
+            return "Not enough capacity in the shop"
+
+        if item_name not in self.items.keys():
+            self.items[item_name] = 1
+        else:
+            self.items[item_name] += 1
+        self.capacity -= 1
+        return f"{item_name} added to the shop"
+
+    def remove_item(self, item_name, amount):
+        if item_name not in self.items.keys() or amount > self.items[item_name]:
+            return f"Cannot remove {amount} {item_name}"
+
+        self.items[item_name] -= amount
+        if self.items[item_name] == 0:
+            del self.items[item_name]
+        return f"{amount} {item_name} removed from the shop"
+
+    def __repr__(self):
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
