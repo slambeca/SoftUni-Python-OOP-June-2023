eb5fb41ae5bd7212074ab9a24e862e808de59055
Borislav Ivanov <slambeca@abv.bg>
2023-06-28 15:04:53 +0300

    Add files via upload

---
+++ 6.1. Polymorphism and Abstraction - Lab/robots.py
@@ -0,0 +1,40 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def sensors_amount():
+        return 1
+
+
+class MedicalRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 6
+
+
+class ChefRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 4
+
+
+class WarRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 12
+
+
+def print_number_of_robot_sensors(robot):
+    print(robot.sensors_amount())
+
+
+basic_robot = Robot('Robo')
+da_vinci = MedicalRobot('Da Vinci')
+moley = ChefRobot('Moley')
+griffin = WarRobot('Griffin')
+
+print_number_of_robot_sensors(basic_robot)
+print_number_of_robot_sensors(da_vinci)
+print_number_of_robot_sensors(moley)
+print_number_of_robot_sensors(griffin)
