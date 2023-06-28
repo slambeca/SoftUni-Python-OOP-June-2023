eb52677ae8a4608709b59fba6a2e32f53c960c36
Borislav Ivanov <slambeca@abv.bg>
2023-06-28 13:16:30 +0300

    Update gym.py

--- 5.2. Class and Static Methods - Exercise/gym/gym.py
+++ 5.2. Class and Static Methods - Exercise/gym/gym.py
@@ -36,9 +36,13 @@ class Gym:
     def subscription_info(self, subscription_id):
         subscription = [s for s in self.subscriptions if s.id == subscription_id][0]
         customer = [c for c in self.customers if c.id == subscription_id][0]
+        # customer = [c for c in self.customers if c.id == subscription.customer_id][0]
         trainer = [t for t in self.trainers if t.id == subscription_id][0]
+        # trainer = [t for t in self.trainers if t.id == subscription.trainer_id][0]
         equipment = [e for e in self.equipment if e.id == subscription_id][0]
+        # equipment = [e for e in self.equipment if e.id == plan.equipment][0]
         plan = [p for p in self.plans if p.id == subscription_id][0]
+        # plan = [p for p in self.plans if p.id == subscription.exercise_id][0]
         result = f"{subscription}\n{customer}\n{trainer}\n{equipment}\n{plan}"
-        
+
         return result
