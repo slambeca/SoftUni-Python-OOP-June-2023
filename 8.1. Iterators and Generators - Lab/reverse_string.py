27387af88b9ac5c0f575642eed2b8102cb21300f
Borislav Ivanov <slambeca@abv.bg>
2023-07-03 11:34:14 +0300

    Second solution added

--- 8.1. Iterators and Generators - Lab/reverse_string.py
+++ 8.1. Iterators and Generators - Lab/reverse_string.py
@@ -5,3 +5,11 @@ def reverse_text(string):
 
 for char in reverse_text("step"):
     print(char, end='')
+
+# Variant 2
+# def reverse_text(string):
+#     yield string[::-1]
+
+
+# for char in reverse_text("step"):
+#     print(char, end='')
