786369b6d859b2e9a44947268e04ced3ab8ae2f6
Borislav Ivanov <slambeca@abv.bg>
2023-07-03 15:59:08 +0300

    Second solution added

--- 9.1. Decorators - Lab/number_increment.py
+++ 9.1. Decorators - Lab/number_increment.py
@@ -11,3 +11,17 @@ def number_increment(numbers):
 
 
 print(number_increment([1, 2, 3]))
+
+# Variant 2
+# def number_increment(numbers):
+#     def increase():
+#         new_numbers = []
+#         for num in numbers:
+#             new_numbers.append(num + 1)
+
+#         return new_numbers
+
+#     return increase
+
+
+# print(number_increment([1, 2, 3])())
