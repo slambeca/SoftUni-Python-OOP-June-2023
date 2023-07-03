ef7a55105bb9faf7124fe8df5f40de994319697b
Borislav Ivanov <slambeca@abv.bg>
2023-07-03 15:33:29 +0300

    Add files via upload

---
+++ 9.1. Decorators - Lab/number_increment.py
@@ -0,0 +1,13 @@
+def number_increment(numbers):
+    def increase():
+        new_numbers = []
+        for num in numbers:
+
+            new_numbers.append(num + 1)
+
+        return new_numbers
+
+    return increase()
+
+
+print(number_increment([1, 2, 3]))
