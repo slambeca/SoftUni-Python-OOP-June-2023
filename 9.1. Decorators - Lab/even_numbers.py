3f3157bb81c2da767847960b2c5c96e22e4ee82f
Borislav Ivanov <slambeca@abv.bg>
2023-07-04 13:36:30 +0300

    Update even_numbers.py

--- 9.1. Decorators - Lab/even_numbers.py
+++ 9.1. Decorators - Lab/even_numbers.py
@@ -1,6 +1,7 @@
 def even_numbers(func_ref):
-    def wrapper(numbers):
-        return [x for x in numbers if x % 2 == 0]
+    def wrapper(*numbers):    # can receive args and kwargs
+        result = func_ref(*numbers)
+        return [x for x in result if x % 2 == 0]
 
     return wrapper
 
