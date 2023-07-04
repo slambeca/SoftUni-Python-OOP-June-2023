fdb822d17996874f5f4d77e722910d3064acbd90
Borislav Ivanov <slambeca@abv.bg>
2023-07-04 11:48:01 +0300

    Add files via upload

---
+++ 9.1. Decorators - Lab/even_numbers.py
@@ -0,0 +1,13 @@
+def even_numbers(func_ref):
+    def wrapper(numbers):
+        return [x for x in numbers if x % 2 == 0]
+
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
