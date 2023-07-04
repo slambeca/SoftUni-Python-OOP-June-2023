113dded5540c27630f38442061f9cd8dd54a532f
Borislav Ivanov <slambeca@abv.bg>
2023-07-04 15:49:29 +0300

    Add files via upload

---
+++ 9.2. Decorators - Exercise/type_check.py
@@ -0,0 +1,29 @@
+def type_check(expected_type):
+    def decorator(func_ref):
+        def wrapper(arg):
+            if isinstance(arg, expected_type):
+                return func_ref(arg)
+
+            return "Bad Type"
+
+        return wrapper
+
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num*2
+
+
+print(times2(2))
+print(times2('Not A Number'))
+
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+
+print(first_letter('Hello World'))
+print(first_letter(['Not', 'A', 'String']))
