bb0f07a209bd4b906a55ce44a6c545c7cdc8f9fa
Borislav Ivanov <slambeca@abv.bg>
2023-07-04 16:15:58 +0300

    Add files via upload

---
+++ 9.2. Decorators - Exercise/cache.py
@@ -0,0 +1,22 @@
+def cache(func_ref):
+    def wrapper(num):
+        if num not in wrapper.log:
+            wrapper.log[num] = func_ref(num)
+
+        return wrapper.log[num]
+
+    wrapper.log = {}
+
+    return wrapper
+
+
+@cache
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+fibonacci(3)
+print(fibonacci.log)
