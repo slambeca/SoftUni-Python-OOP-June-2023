271d9f1c2e0f61fcd0aeca2eb87b3b427a24ac2d
Borislav Ivanov <slambeca@abv.bg>
2023-07-11 18:21:23 +0300

    Update student_report_card.py

--- Final Exams/testing/student_report_card/project/student_report_card.py
+++ Final Exams/testing/student_report_card/project/student_report_card.py
@@ -1,57 +1,75 @@
-from typing import Dict, List
+from project.student_report_card import StudentReportCard
+from unittest import TestCase, main
 
 
-class StudentReportCard:
-    def __init__(self, student_name: str, school_year: int):
-        self.student_name = student_name
-        self.school_year = school_year
-        self.grades_by_subject: Dict[str, List[float]] = {}
+class StudentReportCardTests(TestCase):
+    def setUp(self):
+        self.student_report_card = StudentReportCard("Borislav", 10)
 
-    @property
-    def student_name(self):
-        return self.__student_name
+    def test_correct_initializing(self):
+        self.assertEqual("Borislav", self.student_report_card.student_name)
+        self.assertEqual(10, self.student_report_card.school_year)
+        self.assertEqual({}, self.student_report_card.grades_by_subject)
 
-    @student_name.setter
-    def student_name(self, value):
-        if value == '':
-            raise ValueError("Student Name cannot be an empty string!")
-        self.__student_name = value
+    def test_correct_student_name_with_empty_value_raise_value_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.student_report_card.student_name = ""
 
-    @property
-    def school_year(self):
-        return self.__school_year
+        self.assertEqual("Student Name cannot be an empty string!", str(ve.exception))
 
-    @school_year.setter
-    def school_year(self, value):
-        if not 1 <= value <= 12:
-            raise ValueError("School Year must be between 1 and 12!")
-        self.__school_year = value
+    def test_correct_school_year_with_zero_value_raise_value_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.student_report_card.school_year = 0
 
-    def add_grade(self, subject: str, grade: float):
-        if subject not in self.grades_by_subject:
-            self.grades_by_subject[subject] = []
-        self.grades_by_subject[subject].append(grade)
+        self.assertEqual("School Year must be between 1 and 12!", str(ve.exception))
 
-    def average_grade_by_subject(self):
-        report_for_each_subject = ''
-        for subject, grades in self.grades_by_subject.items():
-            average_grade = sum(grades) / len(grades)
-            report_for_each_subject += f"{subject}: {average_grade:.2f}\n"
-        return report_for_each_subject.strip()
+    def test_correct_school_year(self):
+        self.student_report_card.school_year = 1
 
-    def average_grade_for_all_subjects(self):
-        sum_all_grades = 0
-        all_count = 0
-        for grades in self.grades_by_subject.values():
-            sum_all_grades += sum(grades)
-            all_count += len(grades)
-        return f"Average Grade: {sum_all_grades/ all_count:.2f}"
+        self.assertEqual(1, self.student_report_card.school_year)
 
-    def __repr__(self):
-        report = f"Name: {self.student_name}\n" \
-                 f"Year: {self.school_year}\n" \
-                 f"----------\n" \
-                 f"{self.average_grade_by_subject()}\n" \
-                 f"----------\n" \
-                 f"{self.average_grade_for_all_subjects()}"
-        return report
+    def test_correct_school_year_twelve(self):
+        self.student_report_card.school_year = 12
+
+        self.assertEqual(12, self.student_report_card.school_year)
+
+    def test_add_second_grade_method_with_valid_input(self):
+        self.student_report_card.add_grade("Mathematics", 6.00)
+        self.student_report_card.add_grade("Mathematics", 5.00)
+
+        self.assertEqual({'Mathematics': [6.0, 5.0]}, self.student_report_card.grades_by_subject)
+
+    def test_average_grade_by_subject(self):
+        self.student_report_card.add_grade("Mathematics", 6.00)
+        self.student_report_card.add_grade("Mathematics", 5.00)
+        self.student_report_card.add_grade("Robotics", 6.00)
+        self.student_report_card.add_grade("Robotics", 5.00)
+
+        result = self.student_report_card.average_grade_by_subject()
+
+        self.assertEqual("Mathematics: 5.50\nRobotics: 5.50", result)
+
+    def test_average_grade_for_all_subjects(self):
+        self.student_report_card.add_grade("Mathematics", 6.00)
+        self.student_report_card.add_grade("Mathematics", 5.00)
+        self.student_report_card.add_grade("Electronics", 6.00)
+        self.student_report_card.add_grade("Electronics", 5.00)
+
+        result = self.student_report_card.average_grade_for_all_subjects()
+
+        self.assertEqual("Average Grade: 5.50", result)
+
+    def test_correct__repr__method(self):
+        self.student_report_card.add_grade("Mathematics", 6.00)
+        self.student_report_card.add_grade("Mathematics", 5.00)
+        self.student_report_card.add_grade("Electronics", 6.00)
+        self.student_report_card.add_grade("Electronics", 5.00)
+        result = repr(self.student_report_card)
+        expected_result = "Name: Borislav\nYear: 10\n----------\nMathematics: 5.50\nElectronics: 5.50\n----------\n" \
+                          "Average Grade: 5.50"
+
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == "__main__":
+    main()
