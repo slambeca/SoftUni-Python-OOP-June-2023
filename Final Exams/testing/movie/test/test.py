b6eb189f4d693778d0c0ccc97d74903a4a9fa972
Borislav Ivanov <slambeca@abv.bg>
2023-07-11 19:44:44 +0300

    Second solution added

--- Final Exams/testing/movie/test/test.py
+++ Final Exams/testing/movie/test/test.py
@@ -68,3 +68,67 @@ class MovieTests(TestCase):
 
 if __name__ == "__main__":
     main()
+    
+    
+# Variant 2
+# from project.movie import Movie
+# from unittest import TestCase, main
+# 
+# 
+# class MovieTests(TestCase):
+#     def setUp(self):
+#         self.movie = Movie("Titanic", 1997, 8.5)
+#         self.other_movie = Movie("Robocop", 1998, 8.0)
+# 
+#     def test_correct_initializing(self):
+#         self.assertEqual("Titanic", self.movie.name)
+#         self.assertEqual(1997, self.movie.year)
+#         self.assertEqual(8.5, self.movie.rating)
+#         self.assertEqual([], self.movie.actors)
+# 
+#     def test_correct_name_cannot_be_empty_string_raise_value_error(self):
+#         with self.assertRaises(ValueError) as ve:
+#             self.movie.name = ""
+# 
+#         self.assertEqual("Name cannot be an empty string!", str(ve.exception))
+# 
+#     def test_correct_year_cannot_be_before_1887_raise_value_error(self):
+#         with self.assertRaises(ValueError) as ve:
+#             self.movie.year = 1886
+# 
+#         self.assertEqual("Year is not valid!", str(ve.exception))
+# 
+#     def test_add_actor_which_is_not_already_added(self):
+#         self.movie.add_actor("Maria")
+#         self.movie.add_actor("Josep")
+# 
+#         self.assertEqual(["Maria", "Josep"], self.movie.actors)
+# 
+#     def test_add_actor_which_is_already_added(self):
+#         self.movie.actors = ["Maria"]
+#         result = self.movie.add_actor("Maria")
+# 
+#         self.assertEqual("Maria is already added in the list of actors!", result)
+# 
+#     def test_correct__gt__method_first_movie_has_better_rating(self):
+#         result = self.movie > self.other_movie
+# 
+#         self.assertEqual('"Titanic" is better than "Robocop"', result)
+# 
+#     def test_correct__gt_method_second_movie_has_better_rating(self):
+#         self.other_movie.rating = 10.0
+#         result = self.movie > self.other_movie
+# 
+#         self.assertEqual('"Robocop" is better than "Titanic"', result)
+# 
+#     def test_correct__repr__method(self):
+#         self.movie.add_actor("Maria")
+#         self.movie.add_actor("Josep")
+#         result = str(self.movie)
+#         expected_result = f"Name: Titanic\nYear of Release: 1997\nRating: 8.50\nCast: Maria, Josep"
+# 
+#         self.assertEqual(result, expected_result)
+# 
+# 
+# if __name__ == "__main__":
+#     main()
