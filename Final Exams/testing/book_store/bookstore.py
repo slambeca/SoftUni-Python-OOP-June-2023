24eb2f7a38c036d10ec550ee9b8fc31af7f6df54
Borislav Ivanov <slambeca@abv.bg>
2023-07-11 09:52:59 +0300

    Update bookstore.py

--- Final Exams/testing/book_store/bookstore.py
+++ Final Exams/testing/book_store/bookstore.py
@@ -2,20 +2,20 @@ from typing import Dict
 
 
 class Bookstore:
-    def __init__(self, books_limit: int):    # Done
+    def __init__(self, books_limit: int):    
         self.books_limit = books_limit
         self.availability_in_store_by_book_titles: Dict[str, int] = {}
         self.__total_sold_books: int = 0
 
-    @property    # Done
+    @property    
     def total_sold_books(self):
         return self.__total_sold_books
 
-    @property    # Done
+    @property    
     def books_limit(self):
         return self.__books_limit
 
-    @books_limit.setter    # Done
+    @books_limit.setter    
     def books_limit(self, value: int):
         # the books limit cannot be equal or below zero
         if value <= 0:
@@ -23,7 +23,7 @@ class Bookstore:
         self.__books_limit = value
 
     # count the total number of books (copies) in the bookstore
-    def __len__(self):    # Done
+    def __len__(self):    
         count_books = 0
         for number_of_books in self.availability_in_store_by_book_titles.values():
             count_books += number_of_books
@@ -31,11 +31,11 @@ class Bookstore:
 
     def receive_book(self, book_title: str, number_of_books: int):
         # if there is not enough space in the bookstore
-        if len(self) + number_of_books > self.books_limit:    # Done
+        if len(self) + number_of_books > self.books_limit:   
             raise Exception("Books limit is reached. Cannot receive more books!")
 
         # if there is enough space in the bookstore
-        if book_title not in self.availability_in_store_by_book_titles:    # Done
+        if book_title not in self.availability_in_store_by_book_titles:   
             self.availability_in_store_by_book_titles[book_title] = 0
         self.availability_in_store_by_book_titles[book_title] += number_of_books
 
@@ -45,16 +45,16 @@ class Bookstore:
 
     def sell_book(self, book_title: str, number_of_books: int):
         # if the book is not available in the bookstore
-        if book_title not in self.availability_in_store_by_book_titles:    # Done
+        if book_title not in self.availability_in_store_by_book_titles:    
             raise Exception(f"Book {book_title} doesn't exist!")
 
         # if there is not enough copies of that book to sell
-        if number_of_books > self.availability_in_store_by_book_titles[book_title]:    # Done
+        if number_of_books > self.availability_in_store_by_book_titles[book_title]:   
             books_left = self.availability_in_store_by_book_titles[book_title]
             raise Exception(f"{book_title} has not enough copies to sell. Left: {books_left}")
 
         # if can sell successfully
-        self.availability_in_store_by_book_titles[book_title] -= number_of_books    # Done
+        self.availability_in_store_by_book_titles[book_title] -= number_of_books    
         self.__total_sold_books += number_of_books
         return f"Sold {number_of_books} copies of {book_title}"
 
